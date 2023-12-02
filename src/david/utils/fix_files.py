f2477f66c78b519bc7e6b7ef9db4148a6e764bc2
David Davila-Garcia <2018.ddavila@jburroughs.org>
2023-12-02 17:05:02 -0500

    Add files via upload

--- src/david/utils/fix_files.py
+++ src/david/utils/fix_files.py
@@ -8,23 +8,24 @@ def delete_single_line_json_files(directory):
         # Construct the full file path
         file_path = os.path.join(directory, filename)
         # Check if the file is a JSON file
-        if os.path.isfile(file_path) and filename.endswith(".txt"):
+        if os.path.isfile(file_path) and filename.endswith(".csv"):
             # Open the file and read the lines
             with open(file_path, "r") as file:
                 lines = file.readlines()
                 # Check if the file has only one line
                 if (
                     (len(lines) == 0)
-                    | (len(lines) == 1)
-                    | (len(lines) == 2)
-                    | (len(lines) == 3)
+                    # | (len(lines) == 1)
+                    # | (len(lines) == 2)
+                    # | (len(lines) == 3)
                 ):
                     # Delete the file
                     os.remove(file_path)
-                    print(f"Deleted '{file_path}' as it contains only one line.")
+                    print(f"Deleted '{file_path}' as it contains only zero lines.")
+                elif (len(lines) == 1) | (lines[0] == "\n"):
+                    print(lines)
 
 
-directory = "/home/ddavilag/Desktop/mimic/data"
+directory = "/home/ddavilag/mimic/pretty"
 delete_single_line_json_files(directory)
-print(len(glob.glob("/home/ddavilag/Desktop/mimic/data/*.json")))
-print(len(glob.glob("/home/ddavilag/Desktop/mimic/data/*.txt")))
+print(len(glob.glob("/home/ddavilag/mimic/pretty/*.csv")))
